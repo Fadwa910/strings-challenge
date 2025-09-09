a0a9dcf917212e9ac962e056f354fa77b050241c
Fadwa <okfadwach2@gmail.com>
2025-09-09 12:41:21 +0200

    challenge2

---
+++ challenge2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char t[100];
+    int longeur = 0;
+    int i = 0;
+    printf("entrer fgg : ");
+fgets(t,sizeof(t),stdin); 
+   while(t[i]!='\0'  && t[i]!='\n'){
+       longeur++;
+       i++;
+   }
+    printf("%d\n", longeur);
+    
+    return 0;
+}
